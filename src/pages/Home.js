404fb9c081d0bfe96eaa08ab49c73e9879914640
Pamela <abrepamm@gmail.com>
2022-11-12 18:43:01 -0300

    feat: home page structure

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Pamela Abreu</h1>
+          <h3>Full Stack Developer</h3>
+          <button>SOBRE MIM</button>
+        </div>
+        <div/>
+      </div>
+    );
+  }
+}
+
+export default Home;
