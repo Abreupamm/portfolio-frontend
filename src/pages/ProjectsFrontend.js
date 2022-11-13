86dcb816f1191f018bee34b2dbb3b3d7baa766bb
Pamela <abrepamm@gmail.com>
2022-11-13 18:59:21 -0300

    feat: project front page style created

--- src/pages/ProjectsFrontend.js
+++ src/pages/ProjectsFrontend.js
@@ -1,12 +1,13 @@
 import { Component } from 'react';
 import Aside from '../components/Aside';
 import Title from '../components/Title';
+import '../CSS/frontpage.css';
 
 class ProjectsFrontend extends Component {
   render() {
     const { history } = this.props;
     return (
-      <div>
+      <div className="projects-page">
         <Title title="PROJETOS FRONTEND" />
         <Aside history={history} position="left" />
       </div>
