a75090eea55d0ab1da23fa16261bf3888ca36b4f
Pamela <abrepamm@gmail.com>
2022-11-15 16:04:03 -0300

    feat: details page style

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -71,19 +71,25 @@ class ProjectsDetails extends Component {
       <div className="container-datails">
         <Title title={name} />
         <Aside history={history} />
-        {gif !== '' && <img src={gif} alt={`gif da aplicação ${name}`} />}
-        <p>{description}</p>
-        <span>{`Habilidades: ${skills}`}</span>
-        <button onClick={this.handleOnClick} name="code">
-          Ver Código
-        </button>
-        <button onClick={this.handleOnClick} name="application">
-          Ver Aplicação
-        </button>
-        <div className="div-progress">
-          {keys.map((key, index) => (
-            <LanguageProgress tec={key} value={values[index]} />
-          ))}
+        <div className='container-left'>
+          {gif !== '' && <img src={gif} alt={`gif da aplicação ${name}`} />}
+          <span className="skills">{`Habilidades: ${skills}`}</span>
+        </div>
+        <div className='container-right'>
+          <p>{description}</p>
+          <div className="div-buttons">
+            <button onClick={this.handleOnClick} name="code">
+              Ver Código
+            </button>
+            <button onClick={this.handleOnClick} name="application">
+              Ver Aplicação
+            </button>
+          </div>
+          <div className="div-progress">
+            {keys.map((key, index) => (
+              <LanguageProgress tec={key} value={values[index]} />
+            ))}
+          </div>
         </div>
       </div>
     );
