c1e44b265369bf99ef3ebf7a54ef472410a9812c
Pamela <abrepamm@gmail.com>
2022-11-15 11:26:52 -0300

    refactor: calc progress

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -11,6 +11,18 @@ class ProjectsDetails extends Component {
     values: [],
     keys: [],
   };
+
+  createCalcProgress = async (projectName) => {
+    const getProgress = await fetchLanguageProgress(projectName);
+    let calc = 0;
+    Object.values(getProgress).map((value) => (calc += value));
+    const values = Object.values(getProgress).map((value) =>
+      ((value / calc) * 100).toFixed()
+    );
+    const keys = Object.keys(getProgress);
+    this.setState({ keys, values });
+  };
+
   componentDidMount = async () => {
     const {
       match: {
@@ -21,14 +33,7 @@ class ProjectsDetails extends Component {
     const projectData = data.filter((project) => project.id === JSON.parse(id));
     const { name, projectName } = projectData[0];
     this.setState({ name, projectName });
-
-    const getProgress = await fetchLanguageProgress(projectName);
-    const values = Object.values(getProgress).map(
-      (value) => (value / 100).toFixed() - 2
-    );
-    const keys = Object.keys(getProgress);
-    this.setState({ keys, values });
-    
+    await this.createCalcProgress(projectName);
   };
 
   render() {
@@ -40,11 +45,9 @@ class ProjectsDetails extends Component {
         <Title title={name} />
         <Aside history={history} />
         <div>
-          {
-            keys.map((key, index) => (
-              <LanguageProgress tec={key} value={values[index]} />
-            ))
-          }
+          {keys.map((key, index) => (
+            <LanguageProgress tec={key} value={values[index]} />
+          ))}
         </div>
       </div>
     );
--- src/utils/fetchProjects.js
+++ src/utils/fetchProjects.js
@@ -7,9 +7,12 @@ export const fetchProject = async () => {
 
 export const fetchLanguageProgress = async (projectName) => {
   const projectsAll = await fetchProject();
-  const project = projectsAll.filter((project) => project.name.toLowerCase().includes(projectName));
+  const project = projectsAll.filter((project) =>
+    project.name.toLowerCase().includes(projectName)
+  );
   const url = `https://api.github.com/repos/Abreupamm/${project[0].name}/languages`;
   const req = await fetch(url);
   const response = await req.json();
+  [response].map((element) => delete element['Shell']);
   return response;
 };
