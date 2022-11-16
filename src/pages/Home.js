e68768ab3adce321215da5157a67b65c2efb186b
Pamela <abrepamm@gmail.com>
2022-11-15 23:20:38 -0300

    refactor: page home style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import { Component } from 'react';
 import '../CSS/pages/home.css';
 import Aside from '../components/Aside';
 import Footer from '../components/Footer';
+import { bounceInDown } from 'react-animations';
+import { StyleSheet, css } from 'aphrodite';
 
 class Home extends Component {
   handleOnClick = () => {
@@ -10,14 +12,24 @@ class Home extends Component {
   };
   render() {
     const { history } = this.props;
+    const styles = StyleSheet.create({
+      bounceInDown: {
+        animationName: bounceInDown,
+        animationDuration: '4s',
+      },
+    });
     return (
       <div className="home-page">
-        {/* <div className="container-img"> */}
-          <Aside history={history} />
-        {/* </div> */}
-        <div className="container-name">
+        <Aside history={history} />
+        <img
+          alt=""
+          src="https://cdn.euroinnova.edu.es/img/subidasEditor/alba%20(1)-1624881248.webp"
+        />
+        <div className={`container-name ${css(styles.bounceInDown)}`}>
+          <span className={'sub-title hi1'}>Olá,</span>
+          <span className="sub-title hi2"> meu name é</span>
           <h1 className="name">Pamela Abreu</h1>
-          <h3 className="sub-title">Full Stack Developer</h3>
+          <h3 className="sub-title">Desenvolvedora Full Stack</h3>
           <button onClick={this.handleOnClick} className="button-about">
             SOBRE MIM
           </button>
