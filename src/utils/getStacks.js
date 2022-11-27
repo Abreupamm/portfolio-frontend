c59c7114aa1580703952d68ec5c5ad4cd642e2f9
Pamela <abrepamm@gmail.com>
2022-11-27 16:08:17 -0300

    feat: add project back

--- src/utils/getStacks.js
+++ src/utils/getStacks.js
@@ -8,4 +8,6 @@ export const getStacksImage = {
   node: 'https://cdn-icons-png.flaticon.com/512/5968/5968322.png',
   mysql: 'https://cdn-icons-png.flaticon.com/512/919/919836.png',
   mongodb: 'https://img.icons8.com/color/512/mongodb.png',
+  express: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/7e/Express_Clothing_Logo.SVG/1200px-Express_Clothing_Logo.SVG.png',
+  restApi: 'https://doc.lucidworks.com/assets/images/logos/datasources/rest-api-logo.png',
 }
